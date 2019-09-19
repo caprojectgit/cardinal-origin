c4816519e95dacf7e1f9c83e58d9aa544e5d2e0b
Louis Stephens <lstephens@chumneyads.com>
2019-09-19 16:25:11 -0400

    Flexbox setup
    Move to flexbox from original floats in previous grid.

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ function watch() {
     browserSync.init({
       // You can tell browserSync to use this directory and serve it as a mini-server
       port: 8181,
-        proxy: "http://192.168.12.3:8888/development/projects/cardinal-grid/cardinal-origin"
+        proxy: "http://192.168.12.3:8888/development/projects/cardinal-grid/cardinal-louis"
       // If you are already serving your website locally using something like apache
       // You can use the proxy setting to proxy that instead
       // proxy: "yourlocal.dev"
--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>CSS Grid: Flex</title>
     <!--<link rel="stylesheet" href="dist/css/ca.flex.min.css">-->
-    <link rel="stylesheet" href="dist/css/ca.flex.css">
-    <link rel="stylesheet" href="dist/css/display.css">
+    <link rel="stylesheet" href="dist/css/css.grid.css">
+    <!--    <link rel="stylesheet" href="dist/css/display.css">-->
 
 
 
@@ -14,118 +14,163 @@
 
 <body>
 
-    <section class="nav">
-        <ul>
-            <li><a href="">Grid</a></li>
-            <li><a href="">Item</a></li>
-            <li><a href="">Item</a></li>
-        </ul>
-    </section>
-
-
-
-
-
-
-
-
     <!-- containter -->
-    <div id="container">
+    <div id="ca-container">
+
+        <section>
+            <div class="column   quarter" style="max-width:25%">
+                <div class="acura inner">
+                    Quarter
+                </div>
+            </div>
+            <div class="column  ">
+                <div class="acura inner">
+                    3/4
+                </div>
+            </div>
+        </section>
+
+
+
+
 
         <!-- "row" -->
-        <section>
-            <div class="column bmw">
-                <p class="text-left">Column</p>
+        <section class="align-center">
+
+            <div class="column">
+                <p>Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
+                <p>Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
+
             </div>
 
-            <div class="column bmw">
-                <p class="text-center">Column</p>
+            <div class="column">
+                <p>Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
             </div>
 
-            <div class="column bmw">
-                <p class="text-right">Column</p>
+            <div class="column">
+                <p>Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
             </div>
+            <div class="column">
+                <p>Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
+                <p>Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
+            </div>
+
 
 
         </section>
         <!-- "row" -->
 
-        <!-- "row" -->
+
         <section>
             <div class="column">
-                <a href="#" class="btn">Click Me</a>
+                <a href="#" class="btn bmw alert">Test Button</a>
+            </div>
+            <div class="column">
+                <a href="#" class="btn bmw">Test Button</a>
+            </div>
+            <div class="column">
+                <a href="#" class="btn chevy">Test Button</a>
+            </div>
+            <div class="column">
+                <a href="#" class="btn black">Test Button</a>
+            </div>
+            <div class="column">
+                <a href="#" class="btn black">Test Button</a>
             </div>
 
         </section>
-        <!-- "row" -->
 
 
-
-
-
-
-
-
-
-
-
-
-
-        <!-- "row" -->
-        <!--<form action="">
-                <div class="ca-section">
-                    <div class="column">
-                        <label for="first">First Name</label>
-                        <input type="text" name="first" id="first" required placeholder="First Name">
-                    </div>
-
-
-                    <div class="column">
-                        <label for="last">Last Name</label>
-                        <input type="text" name="last" id="last" required placeholder="Last Name">
-                    </div>
+        <section>
+            <div class="column">
+                <div class="inner acura">
+                    <p>Column</p>
                 </div>
+            </div>
+            <div class="column">
+                <div class="inner buick">
+                    <p>Column</p>
+                </div>
+            </div>
+            <div class="column">
+                <div class="inner buick">
+                    <p>Column</p>
+                </div>
+            </div>
+            <div class="column">
+                <div class="inner toyota">
+                    <p>Column</p>
+                </div>
+            </div>
+            <div class="column">
+                <div class="inner subaru">
+                    <p>Column</p>
+                </div>
+            </div>
+
+        </section>
 
 
-                <div class="ca-section">
-                    <div class="column">
-                        <label for="email">Email Address</label>
-                        <input type="text" name="email" id="email" required placeholder="Email Address">
-                        <input type="hidden" name="emailhoney" id="emailhoney" placeholder="Email Address">
-                    </div>
-                </div>
-                <div class="ca-section">
-                    <div class="column">
-                        <label for="phone">Phone Number</label>
-                        <input type="text" name="phone" id="phone" required placeholder="XXX-XXX-XXXX">
-                    </div>
-                </div>
 
-                <div class="ca-section">
-                    <div class="column">
-                        <label for="contact">Select Box</label>
-                        <select name="contact" id="contact">
-                            <option value="Options">Options</option>
-                            <option value="Options">Options</option>
-                            <option value="Options">Options</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="ca-section">
-                    <div class="column">
-                        <label for="comments">Comments</label>
-                        <textarea type="text" name="comments" id="comments" placeholder="Comments"></textarea>
-                    </div>
-                </div>
 
-                <div class="ca-section">
 
-                    <div class="column">
-                        <button type="submit" id="submit" name="submit" class="btn">Submit</button>
-                    </div>
-                </div>
-            </form>-->
-        <!-- "row" -->
+
+        <!--<section>
+            <div class="column dev">
+                <form>
+
+                    <section>
+                        <div class="column">
+                            <label for="first">First Name</label>
+                            <input type="text" name="first" id="first" required placeholder="First Name">
+                        </div>
+
+                        <div class="column">
+                            <label for="last">Last Name</label>
+                            <input type="text" name="last" id="last" required placeholder="Last Name">
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="column">
+                            <label for="email">Email Address</label>
+                            <input type="text" name="email" id="email" required placeholder="Email Address">
+                            <input type="hidden" name="emailhoney" id="emailhoney" placeholder="Email Address">
+                        </div>
+                        <div class="column">
+                            <label for="phone">Phone Number</label>
+                            <input type="text" name="phone" id="phone" required placeholder="XXX-XXX-XXXX">
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="column">
+                            <label for="contact">Select Box</label>
+                            <select name="contact" id="contact">
+                                <option value="Options">Options</option>
+                                <option value="Options">Options</option>
+                                <option value="Options">Options</option>
+                            </select>
+                        </div>
+                    </section>
+                    <section>
+                        <div class="column">
+                            <label for="comments">Comments</label>
+                            <textarea type="text" name="comments" id="comments" placeholder="Comments"></textarea>
+                        </div>
+                    </section>
+
+                    <section>
+
+                        <div class="column">
+                            <button type="submit" id="submit" name="submit" class="btn acura">Submit</button>
+                        </div>
+
+                    </section>
+                </form>
+
+            </div>
+        </section>-->
 
 
 
